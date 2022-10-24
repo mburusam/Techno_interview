415bc75c772e3959ac221fda364dc7cfc1c6cee2
Samuel Mburu <samuel.mburu@tracom.co.ke>
2022-10-24 10:20:48 +0300

    intial commit

---
+++ src/ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "samuel mburu";
+
+        StringBuilder input1 = new StringBuilder();
+
+        input1.append(input);
+
+        input1.reverse();
+
+        System.out.println(input1);
+    }
+
+}
